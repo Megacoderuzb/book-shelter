50ee68ddd34db71f0276ce371583019a34a11a5c
Muhammadjon Abduvahobov <megacoderuzb@gmail.com>
2023-01-26 11:41:42 +0500

    end film

--- js/login.js
+++ js/login.js
@@ -1,23 +1,22 @@
 let form = document.querySelector("form");
-let email = document.getElementById("exampleInputEmail1");
-let password = document.getElementById("exampleInputPassword1");
+let mail = document.getElementById("exampleInputEmail1");
+let passcode = document.getElementById("exampleInputPassword1");
 let error = document.getElementById("errorTxt");
 error.style.display = "none";
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let login = email.value;
-  let password = password.value;
+
   fetch("https://reqres.in/api/login", {
     method: "POST",
     headers: { "Content-type": "application/json" },
     body: JSON.stringify({
-      password: password,
-      email: login,
+      email: mail.value,
+      password: passcode.value,
     }),
   })
     .then((res) => res.json())
     .then((data) => {
-      if (data.token) {
+      if (data) {
         console.log(data.token);
         window.localStorage.setItem("token", data.token);
         window.location.replace("../index.html");
